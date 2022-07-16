4deab8215e84f5c4c6caac71bacf6baeeeb26d85
Svemat01 <jakob.helgesson@gmail.com>
2022-07-16 22:36:37 +0200

    Fix Linting and add svelte css variables

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,21 +5,29 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'prettier',
-        'plugin:lvksh/recommended',
+        // 'plugin:lvksh/recommended',
+        'plugin:svelte/prettier',
     ],
-    plugins: ['svelte3', '@typescript-eslint', 'lvksh'],
+    plugins: ['@typescript-eslint', 'lvksh'],
     ignorePatterns: ['*.cjs'],
-    overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-    settings: {
-        'svelte3/typescript': () => require('typescript'),
-    },
+    overrides: [
+        {
+            files: ['*.svelte'],
+            parser: 'svelte-eslint-parser',
+            // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+            },
+        },
+    ],
     parserOptions: {
         sourceType: 'module',
         ecmaVersion: 2020,
+        extraFileExtensions: [".svelte"],
     },
     env: {
         browser: true,
         es2017: true,
         node: true,
     },
-};
+}
