0241007566a25998b9f9ec735d3e8dff7c3a23dd
Svemat01 <jakob.helgesson@gmail.com>
2022-07-16 22:32:20 +0200

    Add Jetbrains Mono Font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,11 @@
     import '$lib/styles/app.scss';
 </script>
 
+<svelte:head>
+    <link
+        href="https://fonts.googleapis.com/css?family=JetBrains+Mono"
+        rel="stylesheet"
+    />
+</svelte:head>
+
 <slot />
